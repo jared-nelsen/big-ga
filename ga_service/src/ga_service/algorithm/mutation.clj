5d8e2228b946cea197b193d33ca06e4ecc1449c4
Jared Nelsen <jared.nelsen@gateless.com>
2022-07-22 17:28:54 -0400

    Almost finished mutation

--- ga_service/src/ga_service/algorithm/mutation.clj
+++ ga_service/src/ga_service/algorithm/mutation.clj
@@ -1,1 +1,36 @@
 (ns ga-service.algorithm.mutation)
+
+(defn flipRandomBit
+  [v]
+  (let [randomBitIndex (rand-int (count v))
+        bit (nth v randomBitIndex)
+        flippedBit (if (= 0 bit) 1 0)
+        before (take randomBitIndex v)
+        after (nthrest v (inc randomBitIndex))]
+    (vec (concat (conj before flippedBit) after))))
+;; => #'ga-service.algorithm.mutation/flipRandomBit
+
+
+(defn mutateMember
+  "Mutates the given population member."
+  [member]
+  (let [encoding (:encoding member)
+        mutatedEncoding ()]))
+
+(defn mutatePopulation
+  "Mutates the given population."
+  [population]
+  (loop [population population
+         mutatedPopulation []]
+    (if (empty? population)
+      mutatedPopulation
+      (let [member (first population)
+            mutatedMember (mutateMember member)]
+        (recur (rest population) (conj mutatedPopulation mutatedMember))))))
+
+(defn mutate
+  "Performs the mutation operation of the Genetic Algorithm"
+  [context]
+  (let [population (:population context)
+        mutatedPopulation (mutatePopulation population)]
+    (assoc context :population mutatedPopulation)))
